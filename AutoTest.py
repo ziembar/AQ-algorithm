fb0d76214a53e462ebacc2b9a8ad641a5f9cd4c0
stkwi <stkwiat22a@gmail.com>
2024-06-08 15:11:28 +0200

    NOWE ds

--- AutoTest.py
+++ AutoTest.py
@@ -1,3 +1,5 @@
 from Evaluation import nCV_AQ_CN2_summary, nCV_AQ_summary
 
-nCV_AQ_summary(data='datasets/flights_set.csv', n=3, complex_cut=7)
+
+nCV_AQ_CN2_summary("datasets/flights_set_medium.csv",n=4,complex_cut=1)
+nCV_AQ_CN2_summary("datasets/flights_set_ultimate.csv",n=4,complex_cut=1)
