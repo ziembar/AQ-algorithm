8bc2a35c6150be6feda229cf2057f1131cd70767
stkwi <stkwiat22a@gmail.com>
2024-06-09 11:41:55 +0200

    Testing

--- AQ.py
+++ AQ.py
@@ -27,7 +27,7 @@ class AQ:
 
 
 
-    def __init__(self, training_data, complex_cut=1, binary=False, target=None, scoring1 = 'fast', scoring2 = None, scoring3 = None) -> None:
+    def __init__(self, training_data, complex_cut=1, binary=False, target=None, scoring1 = 'small', scoring2 = None, scoring3 = None) -> None:
         training_examples = []
         with open(training_data, 'r') as file:
             reader = csv.reader(file, delimiter=',')
--- AutoTest.py
+++ AutoTest.py
@@ -1,5 +1,5 @@
 from Evaluation import nCV_AQ_CN2_summary, nCV_AQ_summary
 
 
-nCV_AQ_CN2_summary("datasets/flights_set_medium.csv",n=4,complex_cut=1)
-nCV_AQ_CN2_summary("datasets/flights_set_ultimate.csv",n=4,complex_cut=1)
+nCV_AQ_CN2_summary("datasets/ds_salaries_clean.csv",n=1,complex_cut=1)
+nCV_AQ_summary("datasets/ds_salaries_clean.csv",n=1,complex_cut=3)
